cb7a2b72bd06743e10161ea9d3abcd35f5320d06
mariadamilola <95100757+MariaDamilola@users.noreply.github.com>
2022-08-18 16:44:57 +0100

    add a route to send hello express

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 
+
+
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
@@ -20,6 +22,11 @@ if (!process.env.DISABLE_XORIGIN) {
     next();
   });
 }
+
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
